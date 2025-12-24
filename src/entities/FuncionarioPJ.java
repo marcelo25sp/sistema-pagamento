21bab7ff1df0d6cb744ed5e820fe726373fd3f69
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-23 21:28:59 -0300

    Cria a classe FuncionarioPJ

---
+++ src/entities/FuncionarioPJ.java
@@ -0,0 +1,17 @@
+package entities;
+
+import entities.enums.TipoFuncionario;
+
+public class FuncionarioPJ extends Funcionario {
+
+	private double valorHora;
+	private int horasTrabalhadas;
+
+	public FuncionarioPJ(String nome, double valorHora, int horasTrabalhadas, TipoFuncionario tipo) {
+		super(nome, tipo);
+		this.valorHora = valorHora;
+		this.horasTrabalhadas = horasTrabalhadas;
+
+	}
+
+}
