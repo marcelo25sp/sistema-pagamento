6880aa5d1ed2cd73db7bbe6fa3d2f81c4e5076f3
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-23 23:17:38 -0300

    Implementa uma lista e cria objetos utilizando o polimorfismo das classes na classe Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,15 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Estagiario;
+import entities.Funcionario;
+import entities.FuncionarioCLT;
+import entities.FuncionarioPJ;
+import entities.enums.TipoFuncionario;
 
 public class Main {
 
@@ -9,6 +17,16 @@ public class Main {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		List<Funcionario> funcionarios = new ArrayList<>();
+
+		funcionarios.add(new FuncionarioCLT("João Souza", TipoFuncionario.CLT));
+		funcionarios.add(new FuncionarioPJ("Julia Azevedo", 22.00, 165, TipoFuncionario.PJ));
+		funcionarios.add(new Estagiario("Caio Henrique", 2200.00, TipoFuncionario.ESTAGIARIO));
+
+		System.out.println("==========================");
+		System.out.println("   SISTEMA DE PAGAMENTO   ");
+		System.out.println("==========================");
 
 		sc.close();
 
