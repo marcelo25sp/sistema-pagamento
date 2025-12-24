bd2ac4a681ec19f7b0eefc0246193dfee7892515
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-23 23:20:14 -0300

    Refatora o construtor na classe FuncionarioCLT e adiciona dois atributos

--- src/entities/FuncionarioCLT.java
+++ src/entities/FuncionarioCLT.java
@@ -7,8 +7,10 @@ public class FuncionarioCLT extends Funcionario {
 	private double salarioBase;
 	private double descontoINSS;
 
-	public FuncionarioCLT(String nome, TipoFuncionario tipo) {
+	public FuncionarioCLT(String nome, double salarioBase, double descontoINSS, TipoFuncionario tipo) {
 		super(nome, tipo);
+		this.salarioBase = salarioBase;
+		this.descontoINSS = descontoINSS;
 	}
 
 	@Override
