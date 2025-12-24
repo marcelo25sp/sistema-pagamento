91c3630ccfe3a46eb47542eedaa7922702508d13
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-23 23:32:33 -0300

    Implementa a estrutura de repetição e gera o relatório com os dados dos funcionários

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,13 +20,23 @@ public class Main {
 
 		List<Funcionario> funcionarios = new ArrayList<>();
 
-		funcionarios.add(new FuncionarioCLT("João Souza", TipoFuncionario.CLT));
+		funcionarios.add(new FuncionarioCLT("João Souza", 6000.00, 850.00, TipoFuncionario.CLT));
 		funcionarios.add(new FuncionarioPJ("Julia Azevedo", 22.00, 165, TipoFuncionario.PJ));
 		funcionarios.add(new Estagiario("Caio Henrique", 2200.00, TipoFuncionario.ESTAGIARIO));
 
 		System.out.println("==========================");
 		System.out.println("   SISTEMA DE PAGAMENTO   ");
-		System.out.println("==========================");
+		System.out.println("==========================\n");
+		
+		for(Funcionario f : funcionarios) {
+			System.out.println("Funcionário: " + f.getNome());
+			System.out.println("Tipo: " + f.getTipo());
+			System.out.printf("Pagamento calculado: R$ %.2f%n", f.calcularPagamento());
+			System.out.println("----------------------------");
+		}
+		
+		System.out.println();
+		System.out.println("Processamento finalizado com sucesso!");
 
 		sc.close();
 
