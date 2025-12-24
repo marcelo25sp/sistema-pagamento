0ef3b72baac6e7fa7b26c766ee0a5e9d758ac24e
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-23 21:43:43 -0300

    Implementa o método calcularPagamento na classe FuncionarioCLT

--- src/entities/FuncionarioCLT.java
+++ src/entities/FuncionarioCLT.java
@@ -2,13 +2,18 @@ package entities;
 
 import entities.enums.TipoFuncionario;
 
-public class FuncionarioCLT extends Funcionario{
-	
+public class FuncionarioCLT extends Funcionario {
+
+	private double salarioBase;
+	private double descontoINSS;
+
 	public FuncionarioCLT(String nome, TipoFuncionario tipo) {
 		super(nome, tipo);
 	}
-	
-	private double salarioBase;
-	private double descontoINSS;
+
+	@Override
+	public double calcularPagamento() {
+		return salarioBase - descontoINSS;
+	}
 
 }
