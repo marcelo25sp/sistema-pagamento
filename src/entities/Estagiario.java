bfa6b76100f73d326810ad451f8566e1ebdc5368
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-23 21:32:40 -0300

    Cria a classe Estagiario

---
+++ src/entities/Estagiario.java
@@ -0,0 +1,14 @@
+package entities;
+
+import entities.enums.TipoFuncionario;
+
+public class Estagiario extends Funcionario {
+
+	private double bolsaAuxilio;
+
+	public Estagiario(String nome, double bolsaAuxilio, TipoFuncionario tipo) {
+		super(nome, tipo);
+		this.bolsaAuxilio = bolsaAuxilio;
+	}
+
+}
