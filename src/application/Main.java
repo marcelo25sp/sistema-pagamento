a48ee9b2de55b69084745d27d1b7f2ad0c1db93a
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-29 21:16:28 -0300

    Cria o método auxiliar cadastrarFuncionario na classe Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -23,62 +23,62 @@ public class Main {
 		System.out.println("=========================================");
 		System.out.println("   SISTEMA DE PAGAMENTO JAVA VERSÃO 2.0  ");
 		System.out.println("=========================================\n");
-		
+
 		System.out.println("1. Cadastrar funcionário");
 		System.out.println("2. Listar funcionários");
 		System.out.println("0. Sair");
 		System.out.print("\nEscolha uma opção: ");
 		int opcao = sc.nextInt();
-		sc.nextLine(); 
-		
-		if(opcao == 1) {
+		sc.nextLine();
+
+		if (opcao == 1) {
 			System.out.println("\nEm construção...");
-		}else if(opcao == 2) {
+		} else if (opcao == 2) {
 			System.out.println("\nEm construção...");
-		}else {
+		} else {
 			System.out.println("\nSaindo...");
 		}
-		
+
 		System.out.print("Quantos funcionarios voce deseja cadastrar? ");
 		int n = sc.nextInt();
-		sc.nextLine(); 
+		sc.nextLine();
 
 		for (int i = 1; i <= n; i++) {
 			System.out.print("Nome: ");
 			String nome = sc.nextLine();
 			System.out.print("Tipo de Funcionário (CLT/PJ/ESTAGIARIO): ");
 			TipoFuncionario tipo = TipoFuncionario.valueOf(sc.next().toUpperCase());
-			
-			if(tipo == TipoFuncionario.CLT) {
-				
+
+			if (tipo == TipoFuncionario.CLT) {
+
 				System.out.print("Salário Base:(R$) ");
 				double salarioBase = sc.nextDouble();
 				System.out.print("Desconto do INSS:(R$) ");
-				double descontoINSS = sc.nextDouble();				
+				double descontoINSS = sc.nextDouble();
 				funcionarios.add(new FuncionarioCLT(nome, salarioBase, descontoINSS, tipo));
 			}
-			
-			else if(tipo == TipoFuncionario.PJ) {
-				
+
+			else if (tipo == TipoFuncionario.PJ) {
+
 				System.out.print("Valor por hora:(R$) ");
 				double valorHora = sc.nextDouble();
 				System.out.print("Horas trabalhadas: ");
-				int horas = sc.nextInt();				
+				int horas = sc.nextInt();
 				funcionarios.add(new FuncionarioPJ(nome, valorHora, horas, tipo));
 			}
-			
+
 			else {
 				System.out.print("Bolsa auxílio:(R$) ");
-				double bolsaAuxilio = sc.nextDouble();				
+				double bolsaAuxilio = sc.nextDouble();
 				funcionarios.add(new Estagiario(nome, bolsaAuxilio, tipo));
 			}
-			
+
 			sc.nextLine();
 			System.out.println("Cadastro realizado com sucesso!");
 			System.out.println("----------------------------------------");
-			
+
 		}
-		
+
 		System.out.println("\n========Funcionários cadastrados========\n");
 
 		for (Funcionario f : funcionarios) {
@@ -93,6 +93,42 @@ public class Main {
 
 		sc.close();
 
+	}
+
+	private static void cadastrarFuncionario(List<Funcionario> funcionarios, Scanner sc) {
+
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Tipo de Funcionário (CLT/PJ/ESTAGIARIO): ");
+		TipoFuncionario tipo = TipoFuncionario.valueOf(sc.next().toUpperCase());
+
+		if (tipo == TipoFuncionario.CLT) {
+
+			System.out.print("Salário Base:(R$) ");
+			double salarioBase = sc.nextDouble();
+			System.out.print("Desconto do INSS:(R$) ");
+			double descontoINSS = sc.nextDouble();
+			funcionarios.add(new FuncionarioCLT(nome, salarioBase, descontoINSS, tipo));
+		}
+
+		else if (tipo == TipoFuncionario.PJ) {
+
+			System.out.print("Valor por hora:(R$) ");
+			double valorHora = sc.nextDouble();
+			System.out.print("Horas trabalhadas: ");
+			int horas = sc.nextInt();
+			funcionarios.add(new FuncionarioPJ(nome, valorHora, horas, tipo));
+		}
+
+		else {
+			System.out.print("Bolsa auxílio:(R$) ");
+			double bolsaAuxilio = sc.nextDouble();
+			funcionarios.add(new Estagiario(nome, bolsaAuxilio, tipo));
+		}
+
+		sc.nextLine();
+		System.out.println("Cadastro realizado com sucesso!");
+		System.out.println("----------------------------------------");
 	}
 
 }
