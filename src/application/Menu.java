ceb0223ffb6f2fc1b5583844b87c139364e64e21
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-01-04 17:43:15 -0300

    Adiciona opção para gerar relatório de pagamwntos na classe Menu

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -10,6 +10,7 @@ public class Menu {
 		System.out.println("\n===== MENU =====");
 		System.out.println("1. Cadastrar funcionário");
 		System.out.println("2. Listar funcionários");
+		System.out.println("3. Gerar relatório de pagamentos");
 		System.out.println("0. Sair");
 		System.out.println("------------------------------------------");
 		System.out.print("Escolha uma opção: ");
--- src/services/FuncionarioGerenciamento.java
+++ src/services/FuncionarioGerenciamento.java
@@ -62,5 +62,7 @@ public class FuncionarioGerenciamento {
 			System.out.println("-----------------------------------------------------");
 		}
 	}
+	
+	
 
 }
