176e318c397c6b9315d98c3581c033eaf01a7c69
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-02-05 17:05:19 -0300

    Implementa função para entrada de dados do usuário do tipo String

--- src/utils/InputUtils.java
+++ src/utils/InputUtils.java
@@ -1,5 +1,20 @@
 package utils;
 
+import java.util.Scanner;
+
 public class InputUtils {
+
+	private Scanner sc;
+
+	public InputUtils(Scanner sc) {
+		this.sc = sc;
+	}
+
+	// funções para capturar entrada de dados do usuário
+
+	public String lerString(Scanner mensagem) {
+		System.out.println(mensagem);
+		return sc.nextLine();
+	}
 
 }
