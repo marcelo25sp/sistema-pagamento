9948231e03e5d5cca5b003100354bbfd4b96ac7a
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-02-05 17:10:43 -0300

    Implementa função para entrada de dados do usuário do tipo inteiro

--- src/utils/InputUtils.java
+++ src/utils/InputUtils.java
@@ -12,9 +12,21 @@ public class InputUtils {
 
 	// funções para capturar entrada de dados do usuário
 
-	public String lerString(Scanner mensagem) {
-		System.out.println(mensagem);
+	public String lerString(String mensagem) {
+		System.out.print(mensagem);
 		return sc.nextLine();
+	}
+	
+	public int lerInt(String mensagem) {
+		System.out.print(mensagem);
+		while(!sc.hasNextInt()) { //valida se o número é do tipo inteiro
+			System.out.println("Entrada inválida! Digite um número inteiro:");
+			sc.next();
+			System.out.print(mensagem);
+		}
+		int valor = sc.nextInt();
+		sc.nextLine(); // limpa o buffer
+		return valor;
 	}
 
 }
