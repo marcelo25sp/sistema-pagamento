a467f7ea04d19c215d8c3e8affff4f69df5a69ce
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-03-01 19:17:12 -0300

    Remove método cadastrarFuncionario(Scanner sc) do service

--- src/services/FuncionarioGerenciamento.java
+++ src/services/FuncionarioGerenciamento.java
@@ -15,69 +15,12 @@ public class FuncionarioGerenciamento {
 	}
 
 	private List<Funcionario> funcionarios = new ArrayList<>();
-	
+
 	public void cadastrarFuncionario(FuncionarioDTO dto) {
 		Funcionario funcionario = FuncionarioFactory.criar(dto);
 		funcionarios.add(funcionario);
 	}
-	
-	
-	public void cadastrarFuncionario(Scanner sc) {
 
-	    System.out.println("\nCadastro do(a) funcionário(a):");
-
-	    System.out.print("Nome: ");
-	    String nome = sc.nextLine();
-
-	    System.out.print("Tipo de Funcionário (CLT/PJ/ESTAGIARIO): ");
-	    TipoFuncionario tipo = TipoFuncionario.valueOf(sc.next().toUpperCase());
-
-	    Double salarioBase = null;
-	    Double descontoINSS = null;
-	    Double valorHora = null;
-	    Integer horas = null;
-	    Double bolsaAuxilio = null;
-
-	    if (tipo == TipoFuncionario.CLT) {
-	        System.out.print("Salário Base:(R$) ");
-	        salarioBase = sc.nextDouble();
-	        System.out.print("Desconto do INSS:(R$) ");
-	        descontoINSS = sc.nextDouble();
-	    }
-
-	    else if (tipo == TipoFuncionario.PJ) {
-	        System.out.print("Valor por hora:(R$) ");
-	        valorHora = sc.nextDouble();
-	        System.out.print("Horas trabalhadas: ");
-	        horas = sc.nextInt();
-	    }
-
-	    else {
-	        System.out.print("Bolsa auxílio:(R$) ");
-	        bolsaAuxilio = sc.nextDouble();
-	    }
-
-	    sc.nextLine();
-
-	    //  Usa DTO + Factory
-	    FuncionarioDTO dto = new FuncionarioDTO(
-	            nome,
-	            tipo,
-	            salarioBase,
-	            descontoINSS,
-	            valorHora,
-	            horas,
-	            bolsaAuxilio
-	    );
-
-	    Funcionario funcionario = FuncionarioFactory.criar(dto);
-	    funcionarios.add(funcionario);
-
-	    System.out.println("Cadastro realizado com sucesso!");
-	    System.out.println("------------------------------------------------------------------------\n");
-	}
-
-	
 	public void adicionarFuncionario(Funcionario funcionario) {
 		funcionarios.add(funcionario);
 	}
@@ -90,25 +33,25 @@ public class FuncionarioGerenciamento {
 			System.out.println("--------------------------------------------------------------------");
 		}
 	}
-	
-	public List<PagamentoDTO> gerarRelatorioDTO(){
-		
+
+	public List<PagamentoDTO> gerarRelatorioDTO() {
+
 		List<PagamentoDTO> relatorio = new ArrayList<>();
-		
-		for(Funcionario f : funcionarios) {
+
+		for (Funcionario f : funcionarios) {
 			relatorio.add(new PagamentoDTO(f.getNome(), f.getTipo(), f.calcularPagamento()));
 		}
 		return relatorio;
 	}
-	
+
 	public double calcularTotalFolha(List<PagamentoDTO> relatorio) {
 		double total = 0.0;
-		
-		for(PagamentoDTO p : relatorio) {
+
+		for (PagamentoDTO p : relatorio) {
 			total += p.getValorPagamento();
 		}
 		return total;
-		
+
 	}
 
 	public void gerarRelatorioPagamentos() {
