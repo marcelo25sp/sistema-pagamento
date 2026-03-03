db814c3660619fd9094805af9ad7ff4eb5cda276
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-03-03 19:16:45 -0300

    Adiciona validações de domínio na classe Estagiario

--- src/entities/Estagiario.java
+++ src/entities/Estagiario.java
@@ -8,6 +8,11 @@ public class Estagiario extends Funcionario {
 
 	public Estagiario(String nome, double bolsaAuxilio, TipoFuncionario tipo) {
 		super(nome, tipo);
+
+		if (bolsaAuxilio <= 0.0) {
+			throw new IllegalArgumentException("Valor deve ser maior que R$ 0.00");
+		}
+
 		this.bolsaAuxilio = bolsaAuxilio;
 	}
 
