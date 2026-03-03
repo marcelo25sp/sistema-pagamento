d52ed6d07cc8a14830981be14ec70699e4a5ca55
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-03-02 22:05:27 -0300

    Implementa teste para cálculo de pagamento CLT

--- test/services/FuncionarioGerenciamentoTest.java
+++ test/services/FuncionarioGerenciamentoTest.java
@@ -1,6 +1,7 @@
 package services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +28,23 @@ class FuncionarioGerenciamentoTest {
 
 		assertEquals(1, relatorio.size());
 		assertEquals("João", relatorio.get(0).getNome());
+
+	}
+
+	@Test
+	void deveCalcularPagamentoCLTCorretamente() {
+
+		FuncionarioGerenciamento gerenciamento = new FuncionarioGerenciamento();
+
+		FuncionarioDTO funcionario = new FuncionarioDTO("João", TipoFuncionario.CLT, 3000.0, 200.0, null, null, null);
+
+		gerenciamento.cadastrarFuncionario(funcionario);
+
+		var relatorio = gerenciamento.gerarRelatorioDTO();
+
+		double valorPagamento = relatorio.get(0).getValorPagamento();
+
+		assertEquals(2800.0, valorPagamento);
 
 	}
 
